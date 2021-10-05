72e31b3f6a402a83ae8fd3a0efed7f0913694fb3
Cleiton Osti <cleitonosti@gmail.com>
2021-10-04 22:27:16 -0300

    FEAT: desenvolvido models, views e controllers de login e cadastro de usuários

--- src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,9 +1,61 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Controllers
 {
-    public class IdentidadeController: Controller
+    public class IdentidadeController : Controller
     {
-        
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            // API - Registro
+
+            if (false) return View(usuarioRegistro);
+
+            // Realizar registro no APP
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            // API - Registro
+
+            if (false) return View(usuarioLogin);
+
+            // Realizar registro no APP
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        [HttpGet]
+        [Route("sair")]
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
