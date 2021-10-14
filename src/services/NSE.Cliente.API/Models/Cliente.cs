aece8fdfde8b7e8af3fd615a7feed59b64c83e69
Cleiton Osti <cleitonosti@gmail.com>
2021-10-13 21:11:51 -0300

    FEAT: api clientes definido as entidades

---
+++ src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Cliente.API.Models
+{
+    public class Cliente: Entity, IAggregateRoot
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public bool Excluido { get; private set; }
+        public Endereco Endereco { get; private set; }
+
+        // EF Relation
+        protected Cliente() { }
+
+        public Cliente(string nome, string email, string cpf, bool excluido)
+        {
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+            Excluido = false;
+        }
+    }
+}
