fe913c9d414c2a56c1e3bac86cba6d4843c1c052
Cleiton Osti <cleitonosti@gmail.com>
2021-10-13 21:34:31 -0300

    FEAT: Criado entidades para Email e Cpf e validações e adicionado as mesmas a entidade de cliente

--- src/services/NSE.Cliente.API/Models/Cliente.cs
+++ src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -1,24 +1,36 @@
-﻿using NSE.Core.DomainObjects;
+﻿using System;
+using NSE.Core.DomainObjects;
 
 namespace NSE.Cliente.API.Models
 {
     public class Cliente: Entity, IAggregateRoot
     {
         public string Nome { get; private set; }
-        public string Email { get; private set; }
-        public string Cpf { get; private set; }
+        public Email Email { get; private set; }
+        public Cpf Cpf { get; private set; }
         public bool Excluido { get; private set; }
         public Endereco Endereco { get; private set; }
 
         // EF Relation
         protected Cliente() { }
 
-        public Cliente(string nome, string email, string cpf, bool excluido)
+        public Cliente(Guid id, string nome, string email, string cpf)
         {
+            Id = id;
             Nome = nome;
-            Email = email;
-            Cpf = cpf;
+            Email = new Email(email);
+            Cpf = new Cpf(cpf);
             Excluido = false;
+        }
+
+        public void TrocarEmail(string email)
+        {
+            Email = new Email(email);
+        }
+
+        public void AtribuirEndereco(Endereco endereco)
+        {
+            Endereco = endereco;
         }
     }
 }
