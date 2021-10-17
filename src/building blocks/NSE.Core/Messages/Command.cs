0b3b1e7084335d53e39a1d5b03b0eb91ee8e264b
Cleiton Osti <cleitonosti@gmail.com>
2021-10-17 16:30:53 -0300

    FEAT: implementação de mediatR

--- src/building blocks/NSE.Core/Messages/Command.cs
+++ src/building blocks/NSE.Core/Messages/Command.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using FluentValidation.Results;
+using MediatR;
 
 namespace NSE.Core.Messages
 {
-    public abstract class Command: Message
+    public abstract class Command: Message, IRequest<ValidationResult>
     {
         public DateTime TimeStamp { get; private set; }
         public ValidationResult ValidationResult { get; set; }
--- src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
+++ src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -1,12 +1,31 @@
-﻿namespace NSE.Clientes.API.Application.Commands
-{
-    public class ClienteCommandHandler
-    {
-        public void Manipular(RegistrarClienteCommand message)
-        {
-            // validar cmd
+﻿using FluentValidation.Results;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using NSE.Clientes.API.Models;
+using NSE.Core.Messages;
 
-            // persistir na base
+namespace NSE.Clientes.API.Application.Commands
+{
+    public class ClienteCommandHandler : CommandHandler,
+        IRequestHandler<RegistrarClienteCommand, ValidationResult>
+    {
+        public async Task<ValidationResult> Handle(RegistrarClienteCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.EhValido()) return message.ValidationResult;
+
+            var cliente = new Cliente(message.Id, message.Nome, message.Email, message.Cpf);
+
+            // validaçoes de negocio
+
+            // persistir no banco
+            if (true) // Ja existe cliente com o CPF informado
+            {
+                AdicionarErro("Este CPF já está em uso.");
+                return ValidationResult;
+            }
+
+            return message.ValidationResult;
         }
     }
 }
