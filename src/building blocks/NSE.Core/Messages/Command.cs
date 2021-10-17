7a7499577a811e1a91cf43227566cb02053a7d38
Cleiton Osti <cleitonosti@gmail.com>
2021-10-17 15:58:24 -0300

    FEAT: Adicionado Command e CommandHandler para Api Clientes

---
+++ src/building blocks/NSE.Core/Messages/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentValidation.Results;
+
+namespace NSE.Core.Messages
+{
+    public abstract class Command: Message
+    {
+        public DateTime TimeStamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            TimeStamp = DateTime.Now;
+        }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace NSE.Clientes.API.Application.Commands
+{
+    public class ClienteCommandHandler
+    {
+        public void Manipular(RegistrarClienteCommand message)
+        {
+            // validar cmd
+
+            // persistir na base
+        }
+    }
+}
