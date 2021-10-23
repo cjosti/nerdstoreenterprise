d4fef3a0571374a3be3d93245e7d0e4d1e5ddeab
Cleiton Osti <cleitonosti@gmail.com>
2021-10-23 15:01:49 -0300

    FEAT: validação de CPF com attribute

--- src/web/NSE.WebApp.MVC/Models/UsuarioViewModel.cs
+++ src/web/NSE.WebApp.MVC/Models/UsuarioViewModel.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using NSE.WebApp.MVC.Extensions;
 
 namespace NSE.WebApp.MVC.Models
 {
     public class UsuarioRegistro
     {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DisplayName("Nome Completo")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DisplayName("CPF")]
+        [Cpf]
+        public string Cpf { get; set; }
+
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
         public string Email { get; set; }
